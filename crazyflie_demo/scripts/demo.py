9d4f2cc44d01365b8f06318d4eb34964ba8370a7
zbf <hgkdzbf6@qq.com>
2017-07-24 10:35:39 +0800

    add the circle waypoint, two aircraft can follow it.

--- crazyflie_demo/scripts/demo.py
+++ crazyflie_demo/scripts/demo.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 from tf import TransformListener
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 
 class Demo():
     def __init__(self, goals):
@@ -15,8 +16,17 @@ class Demo():
         self.frame = rospy.get_param("~frame")
         self.pubGoal = rospy.Publisher('goal', PoseStamped, queue_size=1)
         self.listener = TransformListener()
+        rospy.Subscriber("cmd_vel", Twist, self.cmdVelCallback)
         self.goals = goals
+        self.takeoffFlag=0
         self.goalIndex = 0
+        rospy.loginfo("demo start!!!!!!!")
+    
+    def cmdVelCallback(self,data):
+        if data.linear.z != 0.0 and self.takeoffFlag==0:
+            self.takeoffFlag=1
+            rospy.sleep(10)
+            self.takeoffFlag=2
 
     def run(self):
         self.listener.waitForTransform(self.worldFrame, self.frame, rospy.Time(), rospy.Duration(5.0))
@@ -24,27 +34,29 @@ class Demo():
         goal.header.seq = 0
         goal.header.frame_id = self.worldFrame
         while not rospy.is_shutdown():
-            goal.header.seq += 1
-            goal.header.stamp = rospy.Time.now()
-            goal.pose.position.x = self.goals[self.goalIndex][0]
-            goal.pose.position.y = self.goals[self.goalIndex][1]
-            goal.pose.position.z = self.goals[self.goalIndex][2]
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, self.goals[self.goalIndex][3])
-            goal.pose.orientation.x = quaternion[0]
-            goal.pose.orientation.y = quaternion[1]
-            goal.pose.orientation.z = quaternion[2]
-            goal.pose.orientation.w = quaternion[3]
-
+            self.calc_goal(goal,self.goalIndex)
             self.pubGoal.publish(goal)
 
             t = self.listener.getLatestCommonTime(self.worldFrame, self.frame)
             if self.listener.canTransform(self.worldFrame, self.frame, t):
                 position, quaternion = self.listener.lookupTransform(self.worldFrame, self.frame, t)
                 rpy = tf.transformations.euler_from_quaternion(quaternion)
-                if     math.fabs(position[0] - self.goals[self.goalIndex][0]) < 0.3 \
-                   and math.fabs(position[1] - self.goals[self.goalIndex][1]) < 0.3 \
-                   and math.fabs(position[2] - self.goals[self.goalIndex][2]) < 0.3 \
-                   and math.fabs(rpy[2] - self.goals[self.goalIndex][3]) < math.radians(10) \
-                   and self.goalIndex < len(self.goals) - 1:
-                        rospy.sleep(self.goals[self.goalIndex][4])
-                        self.goalIndex += 1
+                if self.takeoffFlag == 1:
+                    self.goalIndex=0
+
+                elif self.takeoffFlag == 2 and self.goalIndex < len(self.goals) - 1:
+                    rospy.sleep(self.goals[self.goalIndex][4]*2)
+                    rospy.loginfo(self.goalIndex)
+                    self.goalIndex += 1
+
+    def calc_goal(self, goal,index):
+        goal.header.seq += 1
+        goal.header.stamp = rospy.Time.now()
+        goal.pose.position.x = self.goals[index][0]
+        goal.pose.position.y = self.goals[index][1]
+        goal.pose.position.z = self.goals[index][2]
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, self.goals[index][3])
+        goal.pose.orientation.x = quaternion[0]
+        goal.pose.orientation.y = quaternion[1]
+        goal.pose.orientation.z = quaternion[2]
+        goal.pose.orientation.w = quaternion[3]
